964d54f2f3b5efa050804e53d09d07b1fbe8f74e
nehamod <nehamo@my.yorku.ca>
2020-08-18 19:45:29 -0400

    Updates

---
+++ precache-manifest.a79f004b3ec36456d4fbf84fba41fe00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f1e24c11ae26b2ed5b55137b0bec8",
+    "url": "/ReactAppJamming/index.html"
+  },
+  {
+    "revision": "95e0c771bdcea4411938",
+    "url": "/ReactAppJamming/static/css/main.7fc568c3.chunk.css"
+  },
+  {
+    "revision": "7a59cd78ea1781494612",
+    "url": "/ReactAppJamming/static/js/2.7da80ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAppJamming/static/js/2.7da80ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e0c771bdcea4411938",
+    "url": "/ReactAppJamming/static/js/main.19702df5.chunk.js"
+  },
+  {
+    "revision": "6bc55a40a27465fb31e7",
+    "url": "/ReactAppJamming/static/js/runtime-main.a334e6dc.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/ReactAppJamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
